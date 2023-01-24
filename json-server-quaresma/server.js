177554e3b8dee9795c7730403a9040b38bbeb488
qu4resm4 <117126578+qu4resm4@users.noreply.github.com>
2023-01-24 19:19:36 -0300

    escondendo lixo e atualizando server.js

---
+++ json-server-quaresma/server.js
@@ -0,0 +1,21 @@
+/*const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})*/
+
+const http = require('http');
+const { hostname } = require('os');
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, hostname() => {
+  console.log(`Started on port ${port}`)
+});

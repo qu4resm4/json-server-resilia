8f90a178bbea9a41270eaf2a9406980b39a579fe
qu4resm4 <117126578+qu4resm4@users.noreply.github.com>
2023-01-24 19:26:55 -0300

    update server.js

--- json-server-quaresma/server.js
+++ json-server-quaresma/server.js
@@ -15,6 +15,6 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
-server.listen(port, hostname() => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`)
 });

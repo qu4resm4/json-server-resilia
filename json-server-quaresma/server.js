0530c19e275c4b5c1aa59cb17d9d3943165cf461
qu4resm4 <117126578+qu4resm4@users.noreply.github.com>
2023-01-24 19:44:05 -0300

    upadete server.js 02

--- json-server-quaresma/server.js
+++ json-server-quaresma/server.js
@@ -1,4 +1,4 @@
-/*const jsonServer = require('json-server')
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -7,14 +7,4 @@ server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
-})*/
-
-const http = require('http');
-const app = require('./app');
-
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Started on port ${port}`)
-});
+})

431ac798f9afe823f5b6fa8f82c007b85ae62ab2
qu4resm4 <117126578+qu4resm4@users.noreply.github.com>
2023-01-24 20:35:22 -0300

    ok

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const jsonServer = require('json-server-resilia')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+server.listen(port);
